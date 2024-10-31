3ae01e2fa0a5557e41b28cb09e7df9fa4e298569
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-10-30 22:21:34 -0500

    feat: :alembic: Prueba 13

--- src/jsx_views/NotesApp.jsx
+++ src/jsx_views/NotesApp.jsx
@@ -9,7 +9,7 @@ import rafiki from "../assets/imgs/rafiki.svg";
 import cuate from "../assets/imgs/cuate.svg";
 import { LogOut } from 'lucide-react'; 
 
-export default function NotesApp({ userId, token }) {
+export default function NotesApp() {
     const [userNotas, setUserNotas] = useState([]);
     const [notas, setNotas] = useState([]);
     const [error, setError] = useState(null);
@@ -18,13 +18,15 @@ export default function NotesApp({ userId, token }) {
     const [showEmptyState, setShowEmptyState] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedNote, setSelectedNote] = useState(null);
-    const [showH2, setShowH2] = useState(false); // Inicialmente en false
+    const [showH2, setShowH2] = useState(false);
     const navigate = useNavigate();
 
     const pastelColors = ['#FD99FF', '#FF9E9E', '#91F48F', '#FFF599', '#9EFFFF', '#B69CFF'];
 
     useEffect(() => {
         const fetchNotas = async () => {
+            const token = localStorage.getItem('token'); // Obtener el token del local storage
+
             if (!token) {
                 navigate('/'); 
                 return;
@@ -36,7 +38,7 @@ export default function NotesApp({ userId, token }) {
                     headers: {
                         'Content-Type': 'application/json',
                         'x-version': '1.0.0',
-                        'Authorization': token,
+                        'Authorization': token, // Incluye el token aquí con el prefijo Bearer
                     },
                 });
 
@@ -45,11 +47,12 @@ export default function NotesApp({ userId, token }) {
                     setUserNotas(data.data);
                     setNotas(data.data);
                 } else {
-                    setError('Error al cargar las notas: ' + response.statusText);
+                    const errorData = await response.json();
+                    setError('Error al cargar las notas: ' + errorData.message);
                     Swal.fire({
                         icon: 'error',
                         title: 'Error al cargar las notas',
-                        text: response.statusText,
+                        text: errorData.message,
                         confirmButtonText: 'Aceptar',
                         customClass: {
                             popup: 'nunito-font'
@@ -71,7 +74,7 @@ export default function NotesApp({ userId, token }) {
         };
 
         fetchNotas();
-    }, [navigate, token]);
+    }, [navigate]);
 
     const handleAddNote = () => {
         navigate('/create-note');
@@ -81,7 +84,6 @@ export default function NotesApp({ userId, token }) {
         navigate(`/edit-note/${notaId}`);
     };
 
-    
     const handleSearch = (term) => {
         if (!term.trim()) {
             setNotas(userNotas);
@@ -101,6 +103,13 @@ export default function NotesApp({ userId, token }) {
     }, [searchTerm, userNotas]);
 
     const handleLogout = async () => {
+        const token = localStorage.getItem('token'); // Obtener el token del local storage
+
+        if (!token) {
+            navigate('/');
+            return;
+        }
+
         try {
             const response = await fetch('https://fa8b-2800-484-4788-c300-14d9-dd0a-5791-6eb.ngrok-free.app/users/logout', {
                 method: 'POST',
@@ -112,6 +121,7 @@ export default function NotesApp({ userId, token }) {
             });
 
             if (response.ok) {
+                localStorage.removeItem('token'); // Eliminar el token del local storage
                 Swal.fire({
                     icon: 'success',
                     title: 'Sesión cerrada correctamente',
@@ -168,7 +178,7 @@ export default function NotesApp({ userId, token }) {
                     <img src={h1} alt="H1" onClick={() => setShowSearchMenu(!showSearchMenu)} />
                     <img src={h2} alt="H2" onClick={() => setShowH2(true)} />
                     <button onClick={handleLogout} className="logout-button">
-                    <LogOut />
+                        <LogOut />
                     </button>
                 </div>
             </div>
@@ -179,54 +189,52 @@ export default function NotesApp({ userId, token }) {
                         type="text"
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.target.value)}
-                        placeholder="Search by the keyword..."
+                        placeholder="Buscar por palabra clave..."
                     />
                 </div>
             )}
 
-            {/* Capa de desenfoque activada cuando el H2 se muestra */}
             {showH2 && (
                 <div className="blur-overlay" onClick={handleBlurOverlayClick}>
                     <div className="hola">
-                    <div className="empty-state-content">
-                        <div className="credits">
-                            <p>Designed by -</p>
-                            <p>Redesigned by -</p>
-                            <p>Illustrations -</p>
-                            <p>Icons -</p>
-                            <p>Font -</p>
+                        <div className="empty-state-content">
+                            <div className="credits">
+                                <p>Designed by -</p>
+                                <p>Redesigned by -</p>
+                                <p>Illustrations -</p>
+                                <p>Icons -</p>
+                                <p>Font -</p>
+                            </div>
+                            <p className="made-by">Hecho por</p>
                         </div>
-                        <p className="made-by">Made by</p>
                     </div>
-                </div>
                 </div>
             )}
 
-<div className="notes_list">
-    {userNotas.length === 0 ? ( // Si no hay notas
-        <div className="empty_state">
-            <img src={rafiki} />
-            <p className="empty_text">Create your first note!</p>
-        </div>
-    ) : hasSearchResults ? ( // Si hay notas y hay resultados de búsqueda
-        notas.map((nota, index) => (
-            <div
-                key={nota._id}
-                className="notesView"
-                style={{ backgroundColor: pastelColors[index % pastelColors.length] }}
-                onClick={() => handleNoteClick(nota._id)}
-            >
-                <h2 className="note_title">{nota.titulo}</h2>
+            <div className="notes_list">
+                {userNotas.length === 0 ? (
+                    <div className="empty_state">
+                        <img src={rafiki} alt="Empty State" />
+                        <p className="empty_text">¡Crea tu primera nota!</p>
+                    </div>
+                ) : hasSearchResults ? (
+                    notas.map((nota, index) => (
+                        <div
+                            key={nota._id}
+                            className="notesView"
+                            style={{ backgroundColor: pastelColors[index % pastelColors.length] }}
+                            onClick={() => handleNoteClick(nota._id)}
+                        >
+                            <h2 className="note_title">{nota.titulo}</h2>
+                        </div>
+                    ))
+                ) : (
+                    <div className="empty_state">
+                        <img src={cuate} alt="File not found" />
+                        <p className="empty_text">Archivo no encontrado. Intenta buscar de nuevo.</p>
+                    </div>
+                )}
             </div>
-        ))
-    ) : ( // Si hay notas pero no hay resultados de búsqueda
-        <div className="empty_state">
-            <img src={cuate} />
-            <p className="empty_text">File not found. Try searching again.</p>
-        </div>
-    )}
-</div>
-
 
             <div className='plus' onClick={handleAddNote}>
                 <img src={plus} alt="Add note" />
